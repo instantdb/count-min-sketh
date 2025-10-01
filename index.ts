27b0a92b980b18567a9e856785f5c7576e6f3ce5
stopachka <stepan.p@gmail.com>
2025-10-01 16:27:19 -0700

    stem

--- index.ts
+++ index.ts
@@ -4,11 +4,32 @@ import fs from "fs";
 const wodehouse = fs.readFileSync("wodehouse.txt", "utf-8");
 
 // 2. Split into words
-function toWords(text: string): string[] {
-  return text.split(" ").map((w) => w.trim().toLowerCase());
+function stem(word: string) {
+  word = word.toLowerCase().replaceAll(/[^a-z]/g, "");
+  if (word.endsWith("ing") && word.length > 4) {
+    word = word.slice(0, -3);
+  } else if (word.endsWith("ed") && word.length > 3) {
+    word = word.slice(0, -2);
+  } else if (word.endsWith("s") && word.length > 3 && !word.endsWith("ss")) {
+    word = word.slice(0, -1);
+  } else if (word.endsWith("ly") && word.length > 3) {
+    word = word.slice(0, -2);
+  } else if (word.endsWith("er") && word.length > 4) {
+    word = word.slice(0, -2);
+  } else if (word.endsWith("est") && word.length > 4) {
+    word = word.slice(0, -3);
+  }
+  return word;
 }
 
-// 3. Exact counts
+function toWords(text: string): string[] {
+  return text
+    .split(" ")
+    .map(stem)
+    .filter((w) => w);
+}
+
+// 3. Get exact counts
 function countWords(words: string[]): { [w: string]: number } {
   const result: { [w: string]: number } = {};
   for (const word of words) {
@@ -17,4 +38,6 @@ function countWords(words: string[]): { [w: string]: number } {
   return result;
 }
 
-console.log("> exact counts", countWords(toWords(wodehouse)));
+const exactCounts = countWords(toWords(wodehouse));
+
+console.log("> exactCounts", exactCounts);
